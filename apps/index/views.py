e33395edada8c8aa74a1faffef4177c16c32a519
Andrew Pogrebnjak <fashust.nefor@gmail.com>
2015-04-04 11:19:12 +0300

    open index page test(p3)

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*- #
 """
     Created on 04.04.15 by fashust
-    index urls patterns
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
-from django.conf.urls import url, patterns
-
-from .views import IndexViewHandler
+from django.views.generic import View
 
 
 __author__ = 'fashust'
 __email__ = 'fashust.nefor@gmail.com'
 
 
-urlpatterns = patterns(
-    'apps.index',
-    url(r'^$', IndexViewHandler.as_view(), name='index'),
+__all__ = (
+    'IndexViewHandler',
 )
+
+
+class IndexViewHandler(View):
+    """
+        simple index page
+    """
